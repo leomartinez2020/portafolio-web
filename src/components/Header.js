4965815dec87b1d5e4cc89ac044880d57529473a
leomartinez2019 <lowenhard@yahoo.com>
2019-04-12 14:19:52 -0500

    Agregar responsive Header component
    - Darle funcionalidad al hamburger menu
    - Crear hook en React para manejar el estado del hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from "react-router-dom";
 import './Header.css';
 
 const Header = () => {
+  const [topnavState, respTopnav] = useState(true);
+
   return (
     <React.Fragment>
-      <div className="topnav" id="myTopnav">
+      <div className={topnavState ? 'topnav' : 'topnav responsive'} id="myTopnav">
         <NavLink to="/" className="active">Inicio</NavLink>
         <NavLink to="acerca.html">Acerca</NavLink>
         <NavLink to="contacto.html">Contacto</NavLink>
-        <a href="javascript:void(0)" className="icon">
+        <a href={"javascript:void(0)"}
+          className="icon" onClick={() => respTopnav(!topnavState)}>
           <i className="fa fa-bars"></i>
         </a>
       </div>
