f7ba605d50f0a305c99b804cf630645896329c27
leomartinez2019 <lowenhard@yahoo.com>
2019-04-13 19:37:10 -0500

    Agregar contenido a las páginas acerca y contacto
    - Agregar íconos
    - Agregar información personal

--- src/components/Acerca.js
+++ src/components/Acerca.js
@@ -4,6 +4,10 @@ const Acerca = () => {
   return (
     <div className="container">
       <h1>Acerca</h1>
+      <p>
+        Desarrollador Python con conocimientos en SQL, Mongodb, JavaScript,
+        Flask, Django y React.
+      </p>
     </div>
   )
 }
--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import './Contacto.css';
 
 const Contacto = () => {
   return (
-    <div className="container">
+    <div className="container contacto">
       <h1>Contacto</h1>
+      <dl>
+        <dt>Correo:</dt>
+        <dd> <i className="far fa-envelope"></i> <a href="mailto:leonardomartinez2019@yahoo.com">leonardomartinez2019@yahoo.com</a></dd>
+
+        <dt>Teléfono:</dt>
+        <dd> <i className="fas fa-phone"></i> 3107489655</dd>
+
+        <dt>Twitter:</dt>
+        <dd> <i className="fab fa-twitter"></i> <a href="https://twitter.com/pythonista100">Pythonista100</a></dd>
+
+        <dt>Ubicación:</dt>
+        <dd> <i class="fas fa-map-marker-alt"></i> Barranquilla</dd>
+        
+      </dl>
     </div>
   )
 }
